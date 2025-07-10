f5b63c32cc121047ffd5ede673202b40d8a17fb4
Tomas Gonzalez <128553827+TomasGLZ@users.noreply.github.com>
2025-07-10 10:46:36 -0300

    Delete src directory

--- src/config/config.java
+++
@@ -1,6 +0,0 @@
-package config;
-
-public class config {
-    public static final String RUTA_BINARIO_PELICULAS = "src/resources/peliculas.dat";
-    public static final String RUTA_CSV_PELICULAS = "src/resources/peliculas.csv";
-}
--- src/model/Genero.java
+++
@@ -1,6 +0,0 @@
-
-package model;
-
-public enum Genero {
-    ACCION, DRAMA, COMEDIA, DOCUMENTAL, TERROR, ANIMACION;
-}
